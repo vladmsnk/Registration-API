3cba88c900edc3c022c99944729d6e7e95d1411f
Андрей Фёдоров <aafyodorov@miem.hse.ru>
2021-10-18 22:03:16 +0300

    Init commit

---
+++ src/test/java/com/selfio/selfio/SelfioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfio.selfio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
