71e31cdad831dd6c014692939549d5ee57708b90
vyumoiseenkov <vyumoiseenkov@miem.hse.ru>
2021-10-24 13:03:40 +0300

    test for UserRepository

---
+++ src/test/java/com/selfio/selfio/UserRepositoryTests.java
@@ -0,0 +1,38 @@
+package com.selfio.selfio;
+
+import com.selfio.selfio.dto.UserRepository;
+import com.selfio.selfio.entities.UserDataObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void testCreateUser() {
+        UserDataObject userDataObject = new UserDataObject();
+        userDataObject.setEmail("vyumoiseenkov@edu.hse.ru");
+        userDataObject.setPassword("qwerty");
+        userDataObject.setVerified(true);
+        UserDataObject savedUser =  userRepository.save(userDataObject);
+        UserDataObject existingUser = testEntityManager.find(UserDataObject.class, savedUser.getId());
+        assertEquals(savedUser, existingUser);
+    }
+}
