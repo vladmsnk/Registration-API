af5fd7f16c74a6a409c5d7c15f4512a52ec4a2c6
vyumoiseenkov <vyumoiseenkov@miem.hse.ru>
2021-10-24 19:24:53 +0300

    add method to register new user account after validation

--- src/main/java/com/selfio/selfio/service/UserService.java
+++ src/main/java/com/selfio/selfio/service/UserService.java
@@ -6,11 +6,10 @@ import com.selfio.selfio.entities.UserDataObject;
 import com.selfio.selfio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements IUserService {
     private final UserRepository userRepository;
 
     @Autowired
@@ -26,11 +25,14 @@ public class UserService {
         );
         return userRepository.save(userDataObject);
     }
-//    public List<UserDataObject> getUsers() {
-//        return userRepository.findAll();
-//    }
 
-//    public Void processRegistration(UserDataObject userDataObject) {
-//        userRepository.save(userDataObject);
-//    }
+    @Override
+    public UserDataObject registerNewUserAccount(UserRegistrationDto userRegistrationDto) {
+        UserDataObject userDataObject = new UserDataObject();
+        userDataObject.setEmail(userRegistrationDto.getEmail());
+        userDataObject.setPassword(userRegistrationDto.getPassword());
+
+        return userRepository.save(userDataObject);
+    }
+
 }
