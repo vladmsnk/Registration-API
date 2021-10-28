6507719d87910f6db159588b2f148571f64cc7fd
vyumoiseenkov <vyumoiseenkov@miem.hse.ru>
2021-10-28 21:08:08 +0300

    Allow all api guests to get in registration form

--- src/main/java/com/selfio/selfio/security/config/WevSecurityConfig.java
+++ src/main/java/com/selfio/selfio/security/config/WevSecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+//
 @Configuration
 @EnableWebSecurity
 public class WevSecurityConfig  extends WebSecurityConfigurerAdapter {
@@ -16,27 +19,23 @@ public class WevSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("registration/**")
+                .antMatchers("/registration/**")
                 .permitAll()
-                .anyRequest()
-                .authenticated()
                 .and()
                 .formLogin();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider()
+        auth.inMemoryAuthentication()
+                .withUser("vlad")
+                .password("1234")
+                .roles("USER");
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider =
-                new DaoAuthenticationProvider();
-        provider.setPasswordEncoder();
-        provider.setUserDetailsService();
-        return provider;
+    PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
